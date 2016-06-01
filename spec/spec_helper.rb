52cdc1fddb42096c59db609365266c72af5e87bc
Felix Krause <KrauseFx@users.noreply.github.com>
2016-06-01 08:33:56 -0700

    Added loading of fastlane actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,5 @@ end
 
 require 'fastlane' # to import the Action super class
 require 'fastlane/plugin/sentry' # import the actual plugin
+
+Fastlane.load_actions # load other actions (in case your plugin calls other actions or shared values)
