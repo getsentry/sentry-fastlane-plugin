6d453fa20adfbae414aae8844de40f445eafd41e
William Schurman <wschurman@gmail.com>
2017-04-23 22:49:16 -0700

    Move sentry API ENV config set to helper
    Also, move org and project slug to be ENV variables rather than shell args

--- lib/fastlane/plugin/sentry/actions/sentry_upload_dsym.rb
+++ lib/fastlane/plugin/sentry/actions/sentry_upload_dsym.rb
@@ -3,34 +3,11 @@ module Fastlane
     class SentryUploadDsymAction < Action
       def self.run(params)
         Helper::SentryHelper.check_sentry_cli!
-
-        # Params - API
-        url = params[:url]
-        auth_token = params[:auth_token]
-        api_key = params[:api_key]
-        org = params[:org_slug]
-        project = params[:project_slug]
+        Helper::SentryConfig.parse_api_params(params)
 
         # Params - dSYM
         dsym_path = params[:dsym_path]
         dsym_paths = params[:dsym_paths] || []
-
-        has_api_key = !api_key.to_s.empty?
-        has_auth_token = !auth_token.to_s.empty?
-
-        # Will fail if none or both authentication methods are provided
-        if !has_api_key && !has_auth_token
-          UI.user_error!("No API key or authentication token found for SentryAction given, pass using `api_key: 'key'` or `auth_token: 'token'`")
-        elsif has_api_key && has_auth_token
-          UI.user_error!("Both API key and authentication token found for SentryAction given, please only give one")
-        elsif has_api_key && !has_auth_token
-          UI.deprecated("Please consider switching to auth_token ... api_key will be removed in the future")
-        end
-
-        ENV['SENTRY_API_KEY'] = api_key unless api_key.to_s.empty?
-        ENV['SENTRY_AUTH_TOKEN'] = auth_token unless auth_token.to_s.empty?
-        ENV['SENTRY_URL'] = url unless url.to_s.empty?
-        ENV['SENTRY_LOG_LEVEL'] = 'debug' if FastlaneCore::Globals.verbose?
 
         # Verify dsym(s)
         dsym_paths += [dsym_path] unless dsym_path.nil?
@@ -41,10 +18,7 @@ module Fastlane
 
         UI.success("sentry-cli #{Fastlane::Sentry::CLI_VERSION} installed!")
 
-        require 'shellwords'
-        org = Shellwords.escape(org)
-        project = Shellwords.escape(project)
-        command = "sentry-cli upload-dsym '#{dsym_paths.join("','")}' --org #{org} --project #{project}"
+        command = "sentry-cli upload-dsym '#{dsym_paths.join("','")}'"
 
         Helper::SentryHelper.call_sentry_cli(command)
         UI.success("Successfully uploaded dSYMs!")
@@ -67,32 +41,7 @@ module Fastlane
       end
 
       def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :url,
-                                       env_name: "SENTRY_URL",
-                                       description: "Url for Sentry",
-                                       is_string: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :auth_token,
-                                       env_name: "SENTRY_AUTH_TOKEN",
-                                       description: "Authentication token for Sentry",
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :api_key,
-                                       env_name: "SENTRY_API_KEY",
-                                       description: "API key for Sentry",
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :org_slug,
-                                       env_name: "SENTRY_ORG_SLUG",
-                                       description: "Organization slug for Sentry project",
-                                       verify_block: proc do |value|
-                                         UI.user_error!("No organization slug for SentryAction given, pass using `org_slug: 'org'`") unless value and !value.empty?
-                                       end),
-          FastlaneCore::ConfigItem.new(key: :project_slug,
-                                       env_name: "SENTRY_PROJECT_SLUG",
-                                       description: "Project slug for Sentry",
-                                       verify_block: proc do |value|
-                                         UI.user_error!("No project slug for SentryAction given, pass using `project_slug: 'project'`") unless value and !value.empty?
-                                       end),
+        Helper::SentryConfig.common_api_config_items + [
           FastlaneCore::ConfigItem.new(key: :dsym_path,
                                        env_name: "SENTRY_DSYM_PATH",
                                        description: "Path to your symbols file. For iOS and Mac provide path to app.dSYM.zip",
