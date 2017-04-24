2e4b7b0be64a9182715a5719b1b587b0a7c67347
William Schurman <wschurman@gmail.com>
2017-04-23 22:17:53 -0700

    Move check for CLI out to helper module

--- lib/fastlane/plugin/sentry/actions/sentry_upload_dsym.rb
+++ lib/fastlane/plugin/sentry/actions/sentry_upload_dsym.rb
@@ -2,7 +2,7 @@ module Fastlane
   module Actions
     class SentryUploadDsymAction < Action
       def self.run(params)
-        check_sentry_cli!
+        Helper::SentryHelper.check_sentry_cli!
 
         # Params - API
         url = params[:url]
@@ -42,25 +42,6 @@ module Fastlane
         UI.success("sentry-cli #{Fastlane::Sentry::CLI_VERSION} installed!")
         call_sentry_cli(dsym_paths, org, project)
         UI.success("Successfully uploaded dSYMs!")
-      end
-
-      def self.check_sentry_cli!
-        if !`which sentry-cli`.include?('sentry-cli')
-          UI.error("You have to install sentry-cli version #{Fastlane::Sentry::CLI_VERSION} to use this plugin")
-          UI.error("")
-          UI.error("Install it using:")
-          UI.command("brew install getsentry/tools/sentry-cli")
-          UI.error("OR")
-          UI.command("curl -sL https://sentry.io/get-cli/ | bash")
-          UI.error("If you don't have homebrew, visit http://brew.sh")
-          UI.user_error!("Install sentry-cli and start your lane again!")
-        end
-
-        sentry_cli_version = Gem::Version.new(`sentry-cli --version`.scan(/(?:\d+\.?){3}/).first)
-        required_version = Gem::Version.new(Fastlane::Sentry::CLI_VERSION)
-        if sentry_cli_version < required_version
-          UI.user_error!("Your sentry-cli is outdated, please upgrade to at least version #{Fastlane::Sentry::CLI_VERSION} and start your lane again!")
-        end
       end
 
       def self.call_sentry_cli(dsym_paths, org, project)
--- lib/fastlane/plugin/sentry/helper/sentry_helper.rb
+++ lib/fastlane/plugin/sentry/helper/sentry_helper.rb
@@ -1,11 +1,23 @@
 module Fastlane
   module Helper
     class SentryHelper
-      # class methods that you define here become available in your action
-      # as `Helper::SentryHelper.your_method`
-      #
-      def self.show_message
-        UI.message("Hello from the sentry plugin helper!")
+      def self.check_sentry_cli!
+        if !`which sentry-cli`.include?('sentry-cli')
+          UI.error("You have to install sentry-cli version #{Fastlane::Sentry::CLI_VERSION} to use this plugin")
+          UI.error("")
+          UI.error("Install it using:")
+          UI.command("brew install getsentry/tools/sentry-cli")
+          UI.error("OR")
+          UI.command("curl -sL https://sentry.io/get-cli/ | bash")
+          UI.error("If you don't have homebrew, visit http://brew.sh")
+          UI.user_error!("Install sentry-cli and start your lane again!")
+        end
+
+        sentry_cli_version = Gem::Version.new(`sentry-cli --version`.scan(/(?:\d+\.?){3}/).first)
+        required_version = Gem::Version.new(Fastlane::Sentry::CLI_VERSION)
+        if sentry_cli_version < required_version
+          UI.user_error!("Your sentry-cli is outdated, please upgrade to at least version #{Fastlane::Sentry::CLI_VERSION} and start your lane again!")
+        end
       end
     end
   end
