dcd7dbafde916d372d646dcdf084606cef8a4977
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-10-30 10:46:18 +0100

    ref: Bump versions, remove tests

--- spec/sentry_upload_dsym_spec.rb
+++ spec/sentry_upload_dsym_spec.rb
@@ -1,19 +1,7 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "upload dsym" do
-      it "fails with no API key or auth token" do
-        dsym_path_1 = File.absolute_path './assets/SwiftExample.app.dSYM.zip'
-
-        expect do
-          Fastlane::FastFile.new.parse("lane :test do
-            sentry_upload_dsym(
-              org_slug: 'some_org',
-              project_slug: 'some_project',
-              dsym_path: '#{dsym_path_1}')
-          end").runner.execute(:test)
-        end.to raise_error("No API key or authentication token found for SentryAction given, pass using `api_key: 'key'` or `auth_token: 'token'`")
-      end
-
+      
       it "fails with API key and auth token" do
         dsym_path_1 = File.absolute_path './assets/SwiftExample.app.dSYM.zip'
 
