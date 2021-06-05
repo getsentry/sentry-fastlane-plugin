ef7c8e5a01a08fc72d2c911068318cf274d6b902
denrase <denrase@gmail.com>
2021-06-05 09:12:32 +0200

    Just pass on parameters without additional checks

--- spec/sentry_create_deploy_spec.rb
+++ spec/sentry_create_deploy_spec.rb
@@ -67,58 +67,33 @@ describe Fastlane do
         end").runner.execute(:test)
       end
 
-      it "fails if started present without finished" do
-        expect do
-          Fastlane::FastFile.new.parse("lane :test do
-              sentry_create_deploy(
-                version: '1.0',
-                env: 'staging',
-                started: 1622630647)
-          end").runner.execute(:test)
-        end.to raise_error("Only one parmeter of 'started' and 'finished' found, please provide both.")
-      end
-
-      it "fails if finished present without started" do
-        expect do
-          Fastlane::FastFile.new.parse("lane :test do
-              sentry_create_deploy(
-                version: '1.0',
-                env: 'staging',
-                finished: 1622630700)
-          end").runner.execute(:test)
-        end.to raise_error("Only one parmeter of 'started' and 'finished' found, please provide both.")
-      end
-
-      it "omit --started and --finished if time if present" do
+      it "includes --started if present" do
         expect(Fastlane::Helper::SentryHelper).to receive(:check_sentry_cli!).and_return(true)
         expect(Fastlane::Helper::SentryConfig).to receive(:parse_api_params).and_return(true)
-        expect(Fastlane::Helper::SentryHelper).to receive(:call_sentry_cli).with(["sentry-cli", "releases", "deploys", "1.0", "new", "--env", "staging", "--time", 180]).and_return(true)
+        expect(Fastlane::Helper::SentryHelper).to receive(:call_sentry_cli).with(["sentry-cli", "releases", "deploys", "1.0", "new", "--env", "staging", "--started", 1622630647]).and_return(true)
 
         Fastlane::FastFile.new.parse("lane :test do
             sentry_create_deploy(
               version: '1.0',
               env: 'staging',
-              time: 180,
-              started: 1622630647,
-              finished: 1622630700)
-        end").runner.execute(:test)
-      end
-
-      it "omit --started if time if present" do
-        expect(Fastlane::Helper::SentryHelper).to receive(:check_sentry_cli!).and_return(true)
-        expect(Fastlane::Helper::SentryConfig).to receive(:parse_api_params).and_return(true)
-        expect(Fastlane::Helper::SentryHelper).to receive(:call_sentry_cli).with(["sentry-cli", "releases", "deploys", "1.0", "new", "--env", "staging", "--time", 180]).and_return(true)
-
-        Fastlane::FastFile.new.parse("lane :test do
-            sentry_create_deploy(
-              version: '1.0',
-              env: 'staging',
-              time: 180,
               started: 1622630647)
         end").runner.execute(:test)
       end
 
-      it "omit --finished if time if present" do
+      it "includes --finished if present" do
+        expect(Fastlane::Helper::SentryHelper).to receive(:check_sentry_cli!).and_return(true)
+        expect(Fastlane::Helper::SentryConfig).to receive(:parse_api_params).and_return(true)
+        expect(Fastlane::Helper::SentryHelper).to receive(:call_sentry_cli).with(["sentry-cli", "releases", "deploys", "1.0", "new", "--env", "staging", "--finished", 1622630700]).and_return(true)
+
+        Fastlane::FastFile.new.parse("lane :test do
+            sentry_create_deploy(
+              version: '1.0',
+              env: 'staging',
+              finished: 1622630700)
+        end").runner.execute(:test)
+      end
+
+      it "includes --time if present" do
         expect(Fastlane::Helper::SentryHelper).to receive(:check_sentry_cli!).and_return(true)
         expect(Fastlane::Helper::SentryConfig).to receive(:parse_api_params).and_return(true)
         expect(Fastlane::Helper::SentryHelper).to receive(:call_sentry_cli).with(["sentry-cli", "releases", "deploys", "1.0", "new", "--env", "staging", "--time", 180]).and_return(true)
@@ -127,8 +102,7 @@ describe Fastlane do
             sentry_create_deploy(
               version: '1.0',
               env: 'staging',
-              time: 180,
-              finished: 1622630700)
+              time: 180)
         end").runner.execute(:test)
       end
     end
