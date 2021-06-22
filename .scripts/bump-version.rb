490aa271180ad74197baf93af17f902d469c2609
Daniel Griesser <daniel.griesser.86@gmail.com>
2021-06-22 15:04:03 +0200

    fix: Bumpscript

--- .scripts/bump-version.rb
+++ .scripts/bump-version.rb
@@ -1,4 +1,4 @@
-file_name "lib/fastlane/plugin/sentry/version.rb"
+file_name = "lib/fastlane/plugin/sentry/version.rb"
 
 text = File.read(file_name)
 new_contents = text.gsub(/VERSION = ".*"/, "VERSION = \"#{ARGV[1]}\"")
