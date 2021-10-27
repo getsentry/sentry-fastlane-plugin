4965fa46e73e382a56e833373534f48af768d1bc
Takeru Chuganji <chu@ganzy.jp>
2021-10-27 11:13:54 -0700

    Update a test case to make sense more

--- spec/sentry_check_cli_installed_spec.rb
+++ spec/sentry_check_cli_installed_spec.rb
@@ -1,8 +1,8 @@
 describe Fastlane do
   describe Fastlane::FastFile do
     describe "check cli installed" do
-      it "success when check_sentry_cli true" do
-        expect(Fastlane::Helper::SentryHelper).to receive(:find_and_check_sentry_cli_path!).and_return('')
+      it "success when find_and_check_sentry_cli_path passes" do
+        expect(Fastlane::Helper::SentryHelper).to receive(:find_and_check_sentry_cli_path!).and_return('path')
 
         Fastlane::FastFile.new.parse("lane :test do
             sentry_check_cli_installed()
--- spec/sentry_helper_spec.rb
+++ spec/sentry_helper_spec.rb
@@ -1,19 +1,20 @@
 describe Fastlane::Helper::SentryHelper do
   describe "call_sentry_cli" do
-    it "uses cli detected by find_and_check_sentry_cli_path!" do
-      options = {
-        sentry_cli_path: 'path'
-      }
-      expect(Fastlane::Helper::SentryHelper).to receive(:find_and_check_sentry_cli_path!).with(options).and_return('sentry-cli')
-      expect(Open3).to receive(:popen3).with("sentry-cli subcommand")
+    it "uses cli path resolved by find_and_check_sentry_cli_path!" do
+      sentry_cli_path = 'path'
+      options = {}
+      expect(Fastlane::Helper::SentryHelper).to receive(:find_and_check_sentry_cli_path!).with(options).and_return(sentry_cli_path)
+      expect(Open3).to receive(:popen3).with("#{sentry_cli_path} subcommand")
+
       Fastlane::Helper::SentryHelper.call_sentry_cli(options, ["subcommand"])
     end
   end
 
   describe "find_and_check_sentry_cli_path!" do
-    it "uses cli path passed" do
-      expect(described_class).to receive(:`).with('path --version').and_return(Fastlane::Sentry::CLI_VERSION)
-      expect(Fastlane::Helper::SentryHelper.find_and_check_sentry_cli_path!({sentry_cli_path: 'path'})).to eq('path')
+    it "uses sentry_cli_path passed to check its version" do
+      sentry_cli_path = 'path'
+      expect(described_class).to receive(:`).with("#{sentry_cli_path} --version").and_return(Fastlane::Sentry::CLI_VERSION)
+      expect(Fastlane::Helper::SentryHelper.find_and_check_sentry_cli_path!({sentry_cli_path: sentry_cli_path})).to eq(sentry_cli_path)
     end
   end
 end
