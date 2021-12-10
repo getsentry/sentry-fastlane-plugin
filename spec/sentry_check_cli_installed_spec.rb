33282a51107f644bee43dfda1e171fb5a2228d81
Takeru Chuganji <chu@ganzy.jp>
2021-12-09 16:28:49 -0800

    Add a test for cli path validation

--- spec/sentry_check_cli_installed_spec.rb
+++ spec/sentry_check_cli_installed_spec.rb
@@ -5,8 +5,20 @@ describe Fastlane do
         expect(Fastlane::Helper::SentryHelper).to receive(:find_and_check_sentry_cli_path!).and_return('path')
 
         Fastlane::FastFile.new.parse("lane :test do
-            sentry_check_cli_installed()
+          sentry_check_cli_installed()
         end").runner.execute(:test)
+      end
+
+      it "raise error on invalid sentry_cli_path" do
+        path = 'invalid_cli_path'
+        expect(FastlaneCore::Helper).to receive(:executable?).and_return(false)
+        expect do
+          Fastlane::FastFile.new.parse("lane :test do
+            sentry_check_cli_installed(
+              sentry_cli_path: '#{path}'
+            )
+          end").runner.execute(:test)
+        end.to raise_error("'#{path}' is not executable")
       end
     end
   end
